a1ad3d0410af5a2d7d12e9f2d83503b641590822
Sahil Bhattacharya <Sahil.b@smartapt.com>
2016-07-01 11:06:24 +0100

    published v 0.0.1

--- RestAjax.js
+++ RestAjax.js
@@ -10,48 +10,110 @@ var RestAjax = function() {
 		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 
-	this.outHeaders = null;
-	this.requestData = null;
+	var outHeaders = null;
+	var requestData = null;
 	
-	this.headers = function(headers) {
-		this.outHeaders = headers;
+	/**
+	 * Public headers(): Set Extra Headers
+	 * @param {Object} headers 
+	 * @return {Null} this
+	 */
+ 	this.headers = function(headers) {
+		outHeaders = headers;
 		return this;
 	}
 
+	/**
+	 * Public send(): Send request data
+	 * @param {Object} data 
+	 * @return {Null} this
+	 */
 	this.send = function(data) {
-		this.requestData = data;
+		requestData = data;
 		return this;
 	}
 
+	/**
+	 * Private transformRequest(): convert json object to url form encoded
+	 * @param {Object} obj 
+	 * @return {Null} this
+	 */
+	var transformRequest = function(obj) {
+        var str = [];
+        for(var key in obj)
+        	str.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]));
+        return str.join("&");
+    }
+
+	/**
+	 * Private SendServerRequest(): convert json object to url form encoded
+	 * @param {String} type 
+	 * @param {String} url
+	 * @return {Null} this
+	 */
+    var SendServerRequest = function(type, url) {
+		xhttp.open(type, url, true);
+
+		if(type == "POST" || type == "PUT")
+			outHeaders['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+		for(header in outHeaders) {
+			xhttp.setRequestHeader(header, outHeaders[header]);
+		}
+    }
+
+	/**
+	 * Public get(): Ajax Get request
+	 * @param {Object} url 
+	 * @return {Null} this
+	 */
 	this.get = function(url) {
-
-		xhttp.open("GET", url, true);
-
-		for(header in this.outHeaders) {
-			xhttp.setRequestHeader(header, this.outHeaders[header]);
-		}
-
+		SendServerRequest('GET', url);
 		return this;
 	}
 
-	this.post = function(url) {
-		xhttp.open("POST", url, true);
-
-		for(header in this.outHeaders) {
-			xhttp.setRequestHeader(header, this.outHeaders[header]);
-		}
-
+	/**
+	 * Public post(): Ajax POST request
+	 * @param {Object} url 
+	 * @return {Null} this
+	 */
+ 	this.post = function(url) {
+		SendServerRequest('POST', url);
 		return this;		
 	}
 
-	this.done = function(callback) {
-		console.log()
-		xhttp.send(this.requestData);
+	/**
+	 * Public put(): Ajax PUT request
+	 * @param {Object} url 
+	 * @return {Null} this
+	 */
+ 	this.put = function(url) {
+		SendServerRequest('PUT', url);
+		return this;		
+	}
+
+	/**
+	 * Public delete(): Ajax DELETE request
+	 * @param {Object} url 
+	 * @return {Null} this
+	 */
+ 	this.delete = function(url) {
+		SendServerRequest('DELETE', url);
+		return this;
+	}
+
+	/**
+	 * Public done(): capturing response and sending it through callback
+	 * @param {Function} callback 
+	 * @return {Null} this
+	 */
+ 	this.done = function(callback) {
+		xhttp.send(transformRequest(requestData));
 		xhttp.onreadystatechange = function() {
-			if (xhttp.readyState == 4 && xhttp.status == 200) {
-				callback(xhttp.responseText);
+			if (xhttp.readyState == 4) {
+				var response = {message:xhttp.responseText, status:xhttp.status};
+				callback(response);
 			}
 		};
-		return this;
 	}
 }
